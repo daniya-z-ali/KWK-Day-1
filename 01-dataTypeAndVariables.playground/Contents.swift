e44da91a2d11726dc2cb17a4e1c4866cb441437a
Daniya Z Ali <daniyazali@Apples-MacBook-Pro-16.local>
2019-08-06 10:29:30 -0400

    Intial commit

---
+++ 01-dataTypeAndVariables.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Examples of string
+"puppies"
+"1234"
+"Hello, world!"
+
+//Example of a variable
+var address = "10 Hudson Yard"
+address = "48th floor"
+var companyName : String
+companyName = "Best Company Name"
+
+//Example of a constant
+let planet = "Earth"
+
+//Examples of joining variables and string
+var string1 = "Welcome"
+var string2 = "Daniya"
+var welcome1 = "\(string1) \(string2)"
+var welcome2 = string1 + " " + string2
+
+//Practice
+print(welcome1)
+print("Here is my sentence")
+var first = "Karlie"
+var  last = "Kloss"
+var answer1 = "\(first)\(last)"
+print(answer1)
+var answer2 = "\(last)\(first)"
+print(answer2)
+var answer3 = "\(first) \(last)"
+print(answer3)
+var answer4a = "\(last) \(first)"
+var answer4 = answer4a + " " + answer4a
+print(answer4)
+var answer5a = "I love"
+var answer5 = answer5a + " " + first
+print(answer5)
+
+//Examples of Integers
+1
+54
+-16
+
+//Examples of Floats & Doubles
+0.13
+3.14
+2.1
+
+var integer = 4
+var double = 5.0
+
+4 * 4
+4 * integer
+4 * double
+
+//Practice with numbers
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+(a + b + c + d)
+206/integer
+print(51)
+a + b * c / d
+a + (b * c) / d
+a + ( (b * c) / d)
+a + b * (c / d)
+(a + b) * (c / d)
+
+
+//Data Type Reviews
+
+//Part1
+//Name - Constant. Although you can change your name, it is something that is given to you to keep.
+//Address - Variable. You will most likely move around as you grow up ans therefore, it is subject to change.
+//Birthday - Constant. There is only one day that you are born.
+//GradeLevel - Variable. You are evolving and growing each and every year.
+//Age - Variable. This is a number that changes every birthday.
+//EyeColor - Constant. Although, you can get colored contacts, you are biologicallly given one eye color.
+//FavoriteColor - Variable. It is one of the choices you make and depending on your experiences and age, this is bound to change.
+
+//Part2
+10.25 * 20
+print(205)
+
+//Part3
+var Name = "Karlie"
+var Part1 = "Happy Birthday to you!"
+var Part2 = "Happy Birthday dear Karlie!"
+var final = " \(Part1) \(Part1) \(Part2) \(Part1) "
+print(final)
---
+++ 02-Conditionals.playground/Contents.swift
@@ -0,0 +1,38 @@
+//Comparison Practice
+5 < 3
+12 > 7
+6 != 8
+"karlie" == "karlie"
+"karlie" == "karliekloss"
+"Karlie" == "karlie"
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+//Example of Conditionals
+var dogAge = 5
+if dogAge < 2 {print("You are a puppy! 🐶")}
+else if dogAge > 12 {print("You are elderly.")}
+else {print("You are awesome!")}
+
+//Control Flow
+var favoriteFood = "Tiramisu"
+if favoriteFood == "Chipotle" {print("Let's get it afterwards!")}
+else if favoriteFood == "Starbucks" {print("Let's get it afterwards!")}
+else if favoriteFood == "Nando's" {print("Let's get it afterwards!")}
+else {print("I want Chocolate!")}
+
+//Control Flow Practice
+//Part1
+var hasAccount = "true"
+if hasAccount == "true" {print("Please log in! Let's get some information to create an account for you.")}
+//Part2
+var gradeLevel = 6
+if gradeLevel < 6 {print("You are in elementary school!")}
+else if gradeLevel > 6 {print("You are in middle school!")}
+else {print("You are in high school!")}
+//Part3
+//if someone were to type in if, then "if" turns purple and is expected to let in "if" condition
+//maybe a conditional?
+// "==" does care about capitalized letters and spaces! This is because code is so specific!
+//Maybe because there are so many little key details to making it correct or to process so it is difficult to understand it.
